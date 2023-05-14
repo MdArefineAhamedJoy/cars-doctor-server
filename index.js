f4c431c03e4e8709192a9128e919d83c045e69e0
Md Arefine Ahamed Joy <mdarefine05@gmail.com>
2023-05-14 12:28:53 +0600

    server operation completed module 70

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express')
-const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
 
-
-// medawar 
-app.use(cors())
-app.use(express.json())
-
-
+// medawar
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.SECRET_KEY}@cluster0.p45io4t.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,28 +17,88 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const servicesCollation = client.db("carDoctor").collection("services");
+    const bookingCollation = client.db("carDoctor").collection("booking");
+
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollation.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, title: 1, price: 1, service_id: 1, img: 1 },
+      };
+      const result = await servicesCollation.findOne(query);
+      res.send(result);
+    });
+
+    // bookings
+
+    app.get("/bookings", async (req, res) => {
+      console.log(req.query?.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query?.email };
+      }
+      const result = await bookingCollation.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollation.insertOne(booking);
+      res.send(result);
+    });
+
+    app.patch("/bookings/:id", async (req, res) => {
+      const update = req.body;
+      const id = req.params.id
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          status:update.status
+        },
+      };
+      const result = await bookingCollation.updateOne(filter, updateDoc)
+      res.send(result)
+    });
+
+
+    app.delete("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollation.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Hello Car Doctor ");
+});
 
-app.get('/', (req , res)=>{
-    res.send('Hello Car Doctor ')
-})
-
-app.listen(port , ()=>{
-    console.log('Car Doctor Server Is Running Port : '+ port)
-})
+app.listen(port, () => {
+  console.log("Car Doctor Server Is Running Port : " + port);
+});
